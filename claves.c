ee524f3e104f7855e3ed07ca97f8b51beac57656
Nathalia Moniz Cordova <nathaliamonizcordova@gmail.com>
2024-02-29 02:25:30 +0100

    primera clase

---
+++ claves.c
@@ -0,0 +1,13 @@
+/* implementacion de servicios */
+#include "claves.h"
+
+
+int init(){
+
+    return 0;
+}
+
+int set_value(int key, char *value, int N_value2, double *V_value2)
+int main(int argc, char *argv[]){
+    
+}
